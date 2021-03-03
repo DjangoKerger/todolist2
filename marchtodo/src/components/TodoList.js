c60325969fc0fc2a355fa711f470fc56b597ed18
“First <Kerger06@aol.com>
2021-03-03 15:56:32 -0600

    frame of to do list

--- marchtodo/src/components/TodoList.js
+++ marchtodo/src/components/TodoList.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoForm from './TodoForm'
 
-const TodoList() {
+const TodoList = () => {
+    const [todos,setTodos] = useState([])
+
+    const addTodo = todo => {
+        if(!todo.text ||/^\s*$/.test(todo.text)) {
+            return
+        }
+
+        const newTodos = [todo, ...todos]
+
+        setTodos(newTodos)
+    }
     return (
         <div>
+            <h1>What's the plan for today</h1>
+            <TodoForm onSubmit={addTodo}/>
         </div>
     )
 }
