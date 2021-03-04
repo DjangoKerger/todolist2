ca43c83e1c087b6e1621c1ff50d350a2f8f1503e
“First <Kerger06@aol.com>
2021-03-04 13:00:35 -0600

    css complete

--- marchtodo/src/components/TodoForm.js
+++ marchtodo/src/components/TodoForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const TodoForm = (props) => {
-    const [input, setInput] = useState('');
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
 
     const handleChange = e => {
         setInput(e.target.value);
